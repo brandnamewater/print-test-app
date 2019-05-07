177a5d940a6f41d0c4cd53d73f0bc31691b17e58
brandnamewater <brandnamewater@gmail.com>
2019-05-06 21:12:42 -0400

    asdf

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,17 @@ class HomeController < ShopifyApp::AuthenticatedController
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
 
-    new_product = ShopifyAPI::Product.new
-    new_product.title = "Burton Custom Freestlye 151"
-    new_product.product_type = "Snowboard"
-    new_product.vendor = "Burton"
-    new_product.save
+    # new_product = ShopifyAPI::Product.new
+    # new_product.title = "Burton Custom Freestlye 151"
+    # new_product.product_type = "Snowboard"
+    # new_product.vendor = "Burton"
+    # new_product.save
+
+
+    ShopifyAPI::Product.new(
+      :title => :title,
+      :vendor => :vendor,
+      :product_type => :product_type,
+    )
   end
 end
