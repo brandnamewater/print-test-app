9a8e62a8263a9585661de27a26a5cd57432bca88
brandnamewater <brandnamewater@gmail.com>
2019-05-07 02:51:37 -0400

    asdf

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_07_055359) do
+ActiveRecord::Schema.define(version: 2019_05_07_065036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.string "shopify_order_id", null: false
@@ -52,6 +58,12 @@ ActiveRecord::Schema.define(version: 2019_05_07_055359) do
     t.datetime "updated_at", null: false
     t.integer "product_id"
     t.string "store_product_id"
+  end
+
+  create_table "styles", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
