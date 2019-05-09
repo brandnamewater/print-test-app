9a77d5513336153a591b908c0eb41c352967c5d6
brandnamewater <brandnamewater@gmail.com>
2019-05-08 20:33:50 -0400

    asdf

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,7 @@ class OrdersController < ShopifyApp::AuthenticatedController
     @orders = Order.all
     @store_products = StoreProduct.all
 
-    @store_orders = ShopifyAPI::Order.find(params: {:line_items[:id] => @store_products.each {|store_product| store_product.store_product_id} })
+    @store_orders = ShopifyAPI::Order.find(params: {:line_items.id => @store_products.each {|store_product| store_product.store_product_id} })
 
     # @match_store_orders = @store_orders.where(id: @store_products.each {|store_product| store_product.store_product_id})
   end
