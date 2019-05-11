a078393ccc55438c1c886984d9a6efe5ec2669a6
brandnamewater <brandnamewater@gmail.com>
2019-05-11 16:15:24 -0400

    asdf

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -48,8 +48,8 @@ class OrdersController < ShopifyApp::AuthenticatedController
     #   end
     # end
 
-    @line_items = @store_orders.each { |order| order.line_items }
-    # @line_items_ids = @line_items.map { |li| li.product_id }
+    @line_items = @store_orders.map { |order| order.line_items }
+    @line_items_ids = @line_items.product_id
     # @common_ids = @store_product_ids && @line_items_ids
 
 
