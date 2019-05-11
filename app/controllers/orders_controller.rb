ed2d8536d430ac006e7c9c9ca7897392ac75c998
brandnamewater <brandnamewater@gmail.com>
2019-05-11 15:33:31 -0400

    asdf

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -27,14 +27,14 @@ class OrdersController < ShopifyApp::AuthenticatedController
 
     @store_product_ids = StoreProduct.pluck(:store_product_id)
 
-    # @store_orders.each do |order|
-    #   @line_items_ = order.line_items
-    #   # @line_items_ids = line_items.collect { |li| li.product_id}
-    #
-    #   # @common_ids = @store_product_ids && @line_items_ids
-    #
-    #
-    # end
+    @store_orders.map do |order|
+      line_items = order.line_items
+      @line_items_ids = line_items.map { |li| li.product_id}
+
+      @common_ids = @store_product_ids && @line_items_ids
+
+
+    end
 
     # @line_items = @store_orders.each do |order|
       # line_items = order.line_items
