b7010106ad2326b5a2aed6fdc5a15a5b5c6ec5fd
brandnamewater <brandnamewater@gmail.com>
2019-05-10 23:22:04 -0400

    asdf

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -29,9 +29,9 @@ class OrdersController < ShopifyApp::AuthenticatedController
 
     @store_orders.each do |order|
       line_items = order.line_items
-      line_items_ids = line_items.map { |li| li.product_id}
+      @line_items_ids = line_items.map { |li| li.product_id}
 
-      @common_ids = @store_product_ids && line_items_ids
+      @common_ids = @store_product_ids && @line_items_ids
 
 
     end
