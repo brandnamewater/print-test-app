8ad58565756335460fedf64681d3319334d1c0aa
brandnamewater <brandnamewater@gmail.com>
2019-05-12 14:25:14 -0400

    adf

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ Rails.application.routes.draw do
 
   # post 'admin/api/products'
 
-  post '/webhook/create_order', to: 'webook#create_order'
+  # post '/webhook/create_order', to: 'webook#create_order'
 
   # post 'shopify_api/products'
 
