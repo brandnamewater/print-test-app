92dab391a8c2807fdce6bafe7f837fdd17204efa
brandnamewater <brandnamewater@gmail.com>
2019-05-21 01:54:31 -0400

    asdf

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,7 +3,7 @@ ShopifyApp.configure do |config|
   config.api_key = "7ca9b191100a7c7618a370e86f31e4ff"
   config.secret = "b5480c0f81a243c526bdb755015beb54"
   config.scope = "read_orders, write_orders, write_products, read_products"
-  config.embedded_app = false
+  config.embedded_app = true
   config.after_authenticate_job = false
   config.session_repository = Shop
 
